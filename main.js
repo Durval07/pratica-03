a0712bb29b93263b4354de34c54bd7599230ea65
Durval <128147337+Durval07@users.noreply.github.com>
2024-03-18 11:17:14 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function introduzirUser(){
+    const text = prompt('Ola qual e seu nome?')
+if(text.length>=6){
+    alert("Seu nome e extenso")
+}
+else{
+    alert("seu nome e pequena")
+  }
+}
+
+introduzirUser()
